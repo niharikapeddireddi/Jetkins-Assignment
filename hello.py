56de600365efee9b7074dbbc64a2324a6d0842cb
Niharika Peddireddi <115718357+niharikapeddireddi@users.noreply.github.com>
2025-05-12 15:40:49 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+import sys
+
+def hello(name="World"):
+    return "Hello %s!" % name
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        print(hello(sys.argv[1]))
+    else:
+        print(hello())
